8984b8d5fac0782f9c53d0e1b23c30a51b1eb53f
wendy <475626671@qq.com>
2021-05-14 15:10:54 +0800

    保存一下

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		自定义组件
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-search",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,23 +1,30 @@
 <template>
-	<view class="container">
-		<scroll-view scroll-y="true" :style="{height:wh+'px'}" class="left-scroll-list">
-			<view v-for="(item, i) in cateList">
-				<view :class="['left-scroll-item',i === active?'active':'']" @click="activeChange(i)">{{item.cat_name}}</view>
-			</view>
-		</scroll-view>
 
-		<scroll-view scroll-y="true" :style="{height:wh+'px'}" class="right-scroll-list" :scroll-top="{scrollTop}">
-			<view class="right-scroll-item" v-for="(item, i) in level2List">
-				<view class="right-scroll-title">/{{item.cat_name}}/</view>
-				<view class="right-scroll-content" >
-				<view class="right-content-image" v-for="(item2, i2) in item.children" @click="navigateToGoods(item2)">
-					<image :src="item2.cat_icon"></image>
-					<text>{{item2.cat_name}}</text>
+	<view class="container" style="flex-direction: column;">
+		<my-search></my-search>
+		<view  class="container">
+			<scroll-view scroll-y="true" :style="{height:wh+'px'}" class="left-scroll-list">
+				<view v-for="(item, i) in cateList">
+					<view :class="['left-scroll-item',i === active?'active':'']" @click="activeChange(i)">
+						{{item.cat_name}}
+					</view>
 				</view>
+			</scroll-view>
+
+			<scroll-view scroll-y="true" :style="{height:wh+'px'}" class="right-scroll-list" :scroll-top="{scrollTop}">
+				<view class="right-scroll-item" v-for="(item, i) in level2List">
+					<view class="right-scroll-title">/{{item.cat_name}}/</view>
+					<view class="right-scroll-content">
+						<view class="right-content-image" v-for="(item2, i2) in item.children"
+							@click="navigateToGoods(item2)">
+							<image :src="item2.cat_icon"></image>
+							<text>{{item2.cat_name}}</text>
+						</view>
+					</view>
 				</view>
-			</view>
-			
-		</scroll-view>
+			</scroll-view>
+		</view>
+
 	</view>
 </template>
 
@@ -27,9 +34,9 @@
 			return {
 				wh: 0,
 				cateList: [],
-				level2List:[],
-				scrollTop:0,
-				active:0,
+				level2List: [],
+				scrollTop: 0,
+				active: 0,
 			};
 		},
 
@@ -47,7 +54,9 @@
 			},
 			// 获取分类数组
 			async getCateList() {
-				const {data: res} = await uni.$http.get('/api/public/v1/categories');
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories');
 				if (res.meta.status !== 200) {
 					return uni.$showMsg('请求失败！')
 				}
@@ -55,19 +64,19 @@
 				this.level2List = res.message[0].children;
 			},
 			// 分类选项改变
-			activeChange(i){
+			activeChange(i) {
 				this.active = i;
 				// 获取二级标题
 				this.level2List = this.cateList[i].children;
-				this.scrollTop = this.scrollTop === 0?1:0;
+				this.scrollTop = this.scrollTop === 0 ? 1 : 0;
 			},
 			//商品页面跳转
-			navigateToGoods(item2){
+			navigateToGoods(item2) {
 				uni.navigateTo({
-					url:'../../subpkg/goods_detail/goods_detail?cid='+item2.cat_id,
+					url: '../../subpkg/goods_detail/goods_detail?cid=' + item2.cat_id,
 				})
 			},
-			
+
 		}
 
 
@@ -103,34 +112,37 @@
 				}
 			}
 		}
-		
-		.right-scroll-list{
-			.right-scroll-title{
+
+		.right-scroll-list {
+			.right-scroll-title {
 				font-size: 12px;
 				font-weight: bold;
 				padding: 15px, 0;
 				text-align: center;
 			}
-			
-			.right-scroll-content{
+
+			.right-scroll-content {
 				width: 100%;
 				display: flex;
 				flex-wrap: wrap;
-				.right-content-image{
+
+				.right-content-image {
 					width: 33.33%;
 					display: flex;
 					flex-direction: column;
 					align-items: center;
 					justify-content: center;
-					image{
+
+					image {
 						width: 60px;
-						height: 60px;				
+						height: 60px;
 					}
-					text{
+
+					text {
 						font-size: 10px;
 					}
 				}
-				
+
 			}
 		}
 
